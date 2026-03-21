a1cdee359b24adfe426f669918b23b23f4f2ed4b
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-03-21 22:13:30 +0600

    the copy

--- class_one/main.py
+++ class_one/main.py
@@ -10,4 +10,19 @@ div= x /y
 print(sum)
 print(sub)
 print(div)
+
+
+
+print("this is first p")
+
+x = 44
+y= 35
+
+sum= x + y
+sub= x -y
+mul= x * y
+div= x /y
+print(sum)
+print(sub)
+print(div)
 print(mul)
